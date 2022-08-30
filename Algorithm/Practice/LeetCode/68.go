cf3b19c33d90e1df63a3aae7dc3288a9efd0f985
roysun <roysun@tencent.com>
2022-08-31 00:35:43 +0800

    feat: 完成部分未完成题目

--- Algorithm/Practice/LeetCode/68.go
+++ Algorithm/Practice/LeetCode/68.go
@@ -1,6 +1,52 @@
 package LeetCode
 
-// 68. Text Justification
-func fullJustify(words []string, maxWidth int) []string {
+import "strings"
 
+// 68. Text Justification
+// blank 返回长度为 n 的由空格组成的字符串
+func blank(n int) string {
+	return strings.Repeat(" ", n)
+}
+
+// left/right qualified的意思是：每行的最左/最右位置不能是空格，必须是单词
+// 当前行是最后一行：单词左对齐（第一个字符非空格），且单词之间应只有一个空格，在行末填充剩余空格；
+// 当前行不是最后一行，且只有一个单词：该单词左对齐，在行末填充空格；
+// 当前行不是最后一行，且不只一个单词：设当前行单词数为numWords，空格数为numSpaces，
+// 我们需要将空格均匀分配在单词之间，则单词之间应至少有avgSpaces = numSpaces / (numWords-1)，结果向下取整
+// 对于多出来的空格数extraSpaces = numSpaces % (numWords-1)，应该填在前extraSpaces个单词之间
+// 因此，前extraSpaces个单词之间填充avgSpaces+1个空格，其余单词之间填充avgSpaces个空格
+func fullJustify(words []string, maxWidth int) (ans []string) {
+	right, n := 0, len(words)
+	for {
+		left := right // 当前行的第一个单词在 words 的位置
+		sumLen := 0   // 统计这一行单词长度之和
+		// 循环确定当前行可以放多少单词，注意单词之间应至少有一个空格
+		for right < n && sumLen+len(words[right])+right-left <= maxWidth {
+			sumLen += len(words[right])
+			right++
+		}
+
+		// 情况1：当前行是最后一行：单词左对齐，且单词之间应只有一个空格，在行末填充剩余空格
+		if right == n {
+			s := strings.Join(words[left:], " ")
+			ans = append(ans, s+blank(maxWidth-len(s)))
+			return
+		}
+
+		numWords := right - left
+		numSpaces := maxWidth - sumLen
+
+		// 情况2：当前行只有一个单词：该单词左对齐，在行末填充剩余空格
+		if numWords == 1 {
+			ans = append(ans, words[left]+blank(numSpaces))
+			continue
+		}
+
+		// 情况3：当前行不只一个单词
+		avgSpaces := numSpaces / (numWords - 1)
+		extraSpaces := numSpaces % (numWords - 1)
+		s1 := strings.Join(words[left:left+extraSpaces+1], blank(avgSpaces+1)) // 拼接额外加一个空格的单词
+		s2 := strings.Join(words[left+extraSpaces+1:right], blank(avgSpaces))  // 拼接其余单词
+		ans = append(ans, s1+blank(avgSpaces)+s2)
+	}
 }
---
+++ Algorithm/Practice/LeetCode/97.go
@@ -0,0 +1,37 @@
+package LeetCode
+
+// 97. Interleaving String
+// 动规
+// 令f(i,j)表示s1的前i个元素和s2的前j个元素是否能交错构成s3的前i+j个元素
+// 如果s1的第i个元素和s3的第i+j个元素相等
+// 那么s1的前i个元素和s2的前j个元素是否能交错构成s3的前i+j个元素取决于
+// s1的前i-1个元素和s2的前j个元素能否交错构成s3的前i+j-1个元素
+// 即此时f(i,j)取决于f(i-1,j)，后者为true则前者也为true
+// 同理，如果s2的第j个元素和s3的第i+j个元素相等并且f(i,j-1)为true，则f(i,j)也为true
+// 于是可推导出状态转移方程：f(i,j) = [f(i-1,j) && s1(i-1) == s3(i+j-1)] || [f(i,j-1) && s2(j-1) == s3(i+j-1)]
+// 其中边界条件为f(0,0)=true
+// f(i,0) = s1(i-1) == s3(i-1) && f(i-1,0)，说明s2为空串，s1要整个等于s3才行
+// f(0,j) = s2(j-1) == s3(j-1) && f(0,j-1)，说明s1为空串，s2要整个等于s3才行
+func isInterleave(s1 string, s2 string, s3 string) bool {
+	n, m, t := len(s1), len(s2), len(s3)
+	if (n + m) != t {
+		return false
+	}
+	dp := make([][]bool, n+1)
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]bool, m+1)
+	}
+	dp[0][0] = true
+	for i := 0; i <= n; i++ {
+		for j := 0; j <= m; j++ {
+			p := i + j - 1
+			if i > 0 {
+				dp[i][j] = dp[i][j] || (dp[i-1][j] && s1[i-1] == s3[p])
+			}
+			if j > 0 {
+				dp[i][j] = dp[i][j] || (dp[i][j-1] && s2[j-1] == s3[p])
+			}
+		}
+	}
+	return dp[n][m]
+}
